49d1b5e6862731618e3b899b75fa53ec60f40007
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-01-13 18:05:24 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+// use std::io;
+// use std::str::FromStr;
+use std::num::ParseFloatError;
+
+pub type Coordinate = (f32, f32, f32);
+pub type XYZLine = (String, Coordinate);
+pub type Snapshot = (String, Vec<XYZLine>);
+
+pub fn parse_coord(x: &str, y: &str, z: &str) -> Result<Coordinate, ParseFloatError> {
+    let vec_x = match x.parse::<f32>() {
+        Ok(value) => value,
+        Err(err) => return Err(err)
+    };
+    let vec_y = match y.parse::<f32>() {
+        Ok(value) => value,
+        Err(err) => return Err(err)
+    };
+    let vec_z = match z.parse::<f32>() {
+        Ok(value) => value,
+        Err(err) => return Err(err)
+    };
+
+    Ok((vec_x, vec_y, vec_z))
+}
+
+// fn read_xyz_line<R: io::BufRead>(reader: &mut R) -> Option<XYZLine> {
+//     let mut buffer = String::new();
+//     try!(reader.read_line(&mut buffer));
+//     let splited: Vec<&str> = buffer.split_whitespace().collect();
+//     if splited.len() == 4 {
+//         Some(
+//             (String::from(splited[0]),
+//             parse_coord(splited[1], splited[2], splited[3]))
+//             )
+//     } else {
+//         None
+//     }
+// }
+
+// fn collect_xyz<R: io::BufRead>(reader: &mut R, num: i32) -> Vec<XYZLine> {
+//     let mut vec = Vec::new();
+//     for _ in 0..num {
+//         match read_xyz_line(reader) {
+//             Some(x) => vec.push(x),
+//             _ => ()
+//         };
+//     }
+//     vec
+// }
+
+// pub fn read_snapshot<R: io::BufRead>(mut reader: &mut R) -> Snapshot {
+//     let mut buffer = String::new();
+//     try!(reader.read_line(&mut buffer));
+//     let num = i32::from_str(&buffer).unwrap(); // TODO try
+//     reader.read_line(&mut buffer); // TODO try
+//     (buffer.clone(), collect_xyz(reader, num))
+// }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
